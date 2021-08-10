c25709425c0b0e9c8b076ec2607f7fa732fbdf57
Michał Długosz <mdugosz@gmail.com>
2021-08-10 18:15:13 +0200

    Add api request and setupImages method

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,40 @@
 import Header from './Header';
 import Footer from './Footer';
+import Content from './Content';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const getData = await fetch('https://api.gwent.one/?key=data&response=json');
+      const data = await getData.json();
+      const images = setupImages(data.response);
+      setImages(images);
+    })();
+  }, []);
+
+  const setupImages = (img) => {
+    const imagesArray = [];
+    const randomNumber = Math.round(Math.random() * 1048);
+
+    for (let i = randomNumber; i < randomNumber + 100; i++) {
+      const image = {
+        url: `https://gwent.one/image/gwent/assets/card/art/medium/${img[i].id.art}.jpg`,
+        name: img[i].name,
+      };
+      imagesArray.push(image);
+    }
+
+    return imagesArray;
+  };
+
+  console.log(images);
   return (
     <div>
       <Header />
+      <Content data={images} />
       <Footer />
     </div>
   );
