c47b5424c75c00579aae6bba14571bdb7d721445
Michał Długosz <mdugosz@gmail.com>
2021-08-13 23:46:20 +0200

    Add content methods and hooks

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Content.css';
+import Card from './Card';
 
-const Content = () => {
-  return null;
+const Content = (props) => {
+  const [data, setData] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  useEffect(() => {
+    getTenImages();
+  }, []);
+
+  const getTenImages = () => {
+    let images = [];
+    for (let i = 0; i < 10; i++) {
+      images.push(props.data[i]);
+    }
+
+    setData(images);
+  };
+
+  const shuffleArray = () => {
+    const newArr = data
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    return newArr;
+  };
+
+  const getCardName = (e) => {
+    const name = e.target.dataset.id;
+    setSelectedImages([...selectedImages, name]);
+    console.log(selectedImages);
+  };
+
+  return (
+    <div className="container">
+      {data.map((image, index) => {
+        return (
+          <Card
+            image={image}
+            key={index}
+            parentState={data}
+            setState={setData}
+            shuffle={shuffleArray}
+            clicked={getCardName}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Content;
