3cfdee593b5cbd537a1cd7eddf7337aea0089640
Michał Długosz <mdugosz@gmail.com>
2021-08-18 19:28:22 +0200

    Add hooks, add givePoint method

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
-import { useState } from 'react';
 import '../styles/Card.css';
+import { useState, useEffect } from 'react';
 
 const Card = (props) => {
+  let [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    props.setState(props.shuffle());
+  }, [counter]);
+
+  const givePoint = () => {
+    setCounter(++counter);
+  };
+
   return (
-    <div>
-      <img alt=""></img>
-      <h5></h5>
+    <div className="card">
+      <img alt="" src={props.image.url} data-id={props.image.name} onClick={givePoint}></img>
+      <h5>{props.image.name}</h5>
     </div>
   );
 };
