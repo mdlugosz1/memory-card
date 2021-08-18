db0afdaadeaa4cb567054461cb18fcadfcfc4d03
Michał Długosz <mdugosz@gmail.com>
2021-08-18 19:28:57 +0200

    Add score content

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,7 +2,16 @@ import { useState } from 'react';
 import '../styles/Score.css';
 
 const Score = (props) => {
-  return null;
+  return (
+    <div>
+      <p>
+        Current score: <span></span>
+      </p>
+      <p>
+        Best score: <span></span>
+      </p>
+    </div>
+  );
 };
 
 export default Score;
