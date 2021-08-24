63dbd57be920e4116a97ec9317677b73d28f17e6
Michał Długosz <mdugosz@gmail.com>
2021-08-24 18:26:13 +0200

    Add classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import '../styles/Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <div>Gwent Memory Card Game</div>
+    <header className="header">
+      <p className="title">Gwent Memory Card Game</p>
     </header>
   );
 };
