dd5c3cdf3e43eb5b972a290b7f5f451c2ac2bc16
Michał Długosz <mdugosz@gmail.com>
2021-08-24 18:26:28 +0200

    Wrap message in container

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,14 +3,16 @@ import '../styles/Message.css';
 const Message = (props) => {
   return (
     <div className="message">
-      {props.score === 100 && (
-        <p>Congratulations! You completed whole game! Refresh page to get new set of cards.</p>
-      )}
-      {props.score < 100 && <p>Keep going!</p>}
-      <button onClick={() => props.setFinished(false)}>
-        {props.score === 100 && 'Close'}
-        {props.score < 100 && 'Try again'}
-      </button>
+      <div className="message-content">
+        {props.score === 100 && (
+          <p>Congratulations! You completed whole game! Refresh page to get new set of cards.</p>
+        )}
+        {props.score < 100 && <p>Keep going!</p>}
+        <button onClick={() => props.setFinished(false)}>
+          {props.score === 100 && 'Close'}
+          {props.score < 100 && 'Try again'}
+        </button>
+      </div>
     </div>
   );
 };
